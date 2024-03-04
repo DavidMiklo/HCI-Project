2e0b42d3ab96d32123c733e72d7478a1eb725254
exegeist <56412437+exegeist@users.noreply.github.com>
2024-03-04 18:43:53 +0100

    fetching prototype

--- src/script.js
+++ src/script.js
@@ -1,20 +1,25 @@
 function updateContent(page) {
-    let message;
-    switch (page) {
-        case 'home':
-            message = "You clicked on the Home button.";
-            break;
-        case 'library':
-            message = "You clicked on the Library button.";
-            break;
-        case 'search':
-            message = "You clicked on the Search button.";
-            break;
-        case 'friends':
-            message = "You clicked on the Friends button.";
-            break;
-        default:
-            message = "Invalid button clicked.";
-    }
-    document.getElementById('content').innerHTML = "<p>" + message + "</p>";
+    fetch(page)
+  .then(response=> response.text())
+  .then(text=> document.getElementById('content').innerHTML = text.getElementById('content').innerHTML);
+
+
+    // let message;
+    // switch (page) {
+    //     case 'home':
+    //         message = "You clicked on the Home button.";
+    //         break;
+    //     case 'library':
+    //         message = "You clicked on the Library button.";
+    //         break;
+    //     case 'search':
+    //         message = "You clicked on the Search button.";
+    //         break;
+    //     case 'friends':
+    //         message = "You clicked on the Friends button.";
+    //         break;
+    //     default:
+    //         message = "Invalid button clicked.";
+    // }
+    // document.getElementById('content').innerHTML = "<p>" + message + "</p>";
 }
