e3f4809c4757e461dde43a7ebc7df4486becfc96
exegeist <56412437+exegeist@users.noreply.github.com>
2024-03-04 18:30:06 +0100

    Branch in script

--- src/script.js
+++ src/script.js
@@ -1,3 +1,20 @@
 function updateContent(page) {
-    document.getElementById('content').innerHTML = "<p>changed</p>";
+    let message;
+    switch (page) {
+        case 'home':
+            message = "You clicked on the Home button.";
+            break;
+        case 'library':
+            message = "You clicked on the Library button.";
+            break;
+        case 'search':
+            message = "You clicked on the Search button.";
+            break;
+        case 'friends':
+            message = "You clicked on the Friends button.";
+            break;
+        default:
+            message = "Invalid button clicked.";
+    }
+    document.getElementById('content').innerHTML = "<p>" + message + "</p>";
 }
